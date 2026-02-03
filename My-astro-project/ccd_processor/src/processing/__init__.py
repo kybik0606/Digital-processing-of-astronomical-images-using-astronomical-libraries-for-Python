ad44c016820715f66eb92c40fe9c2bf4c2c8d23c
Aleksei <kybik0606@gmail.com>
2026-02-04 00:52:56 +0300

    add integritychecker in __init__

--- My-astro-project/ccd_processor/src/processing/__init__.py
+++ My-astro-project/ccd_processor/src/processing/__init__.py
@@ -4,5 +4,6 @@ Image Processing Modules
 
 from .calibration import CalibrationProcessor
 from .masters import MastersProcessor
+from .integrity_checker import IntegrityChecker
 
-__all__ = ['CalibrationProcessor', 'MastersProcessor']
+__all__ = ['CalibrationProcessor', 'MastersProcessor', 'IntegrityChecker']
